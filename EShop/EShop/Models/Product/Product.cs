823bcd3f00134f2e3861c048584e37cf6b865665
Denis <dennis.semionov@gmail.com>
2018-05-26 18:34:30 +0300

    Product model upgrade

--- EShop/EShop/Models/Product/Product.cs
+++ EShop/EShop/Models/Product/Product.cs
@@ -7,13 +7,19 @@ using System.Threading.Tasks;
 
 namespace EShop.Models
 {
-    public class Product: IEquatable<Product>
+    public class Product : IEquatable<Product>
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Product name is required.")]
+        [MaxLength(100, ErrorMessage = "Max {0} length is {1}")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Description is required field.")]
+        [MaxLength(100, ErrorMessage = "Max {0} length is {1}")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, Double.MaxValue, ErrorMessage = "Price cannot be less than 0.01")]
         public Decimal Price { get; set; }
 
         [Timestamp]
